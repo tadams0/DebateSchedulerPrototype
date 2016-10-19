05d2ee28c53e8f2cba925592f9bd2938f2b0a3a4
Taylor <tdadams0@gmail.com>
2016-10-19 19:51:49 -0400

    -Removed webpage magins... -Added and aligned a login module -Footer and header has been expanded -Font-families in the mainstyle.css has been added.

--- DebateScheduler/Default.aspx
+++ DebateScheduler/Default.aspx
@@ -2,4 +2,10 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="mainContent" runat="server">
+
+    <p>
+    &nbsp;</p>
+<asp:Panel ID="Panel1" runat="server" Height="500px" style="margin-top: 0px">
+</asp:Panel>
+
 </asp:Content>
--- DebateScheduler/Default.aspx.designer.cs
+++ DebateScheduler/Default.aspx.designer.cs
@@ -11,5 +11,14 @@ namespace DebateScheduler {
     
     
     public partial class Default {
+        
+        /// <summary>
+        /// Panel1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel1;
     }
 }

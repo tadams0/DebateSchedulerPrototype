f64047c9befb59fbb015e0474606f3623c6aa5fa
Taylor <tdadams0@gmail.com>
2016-10-19 22:23:50 -0400

    Added a database Added two test users for testing logging in/out and permissions Added a method for logging exceptions in the database handler. Added various database handler stuff. Authentication for username/password has been added, but no session is created.

--- DebateScheduler/DatabaseHandler.cs
+++ DebateScheduler/DatabaseHandler.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -12,6 +15,11 @@ namespace DebateScheduler
     /// </summary>
     public static class DatabaseHandler
     {
+        /// <summary>
+        /// The path where exceptions are logged.
+        /// </summary>
+        private static string exceptionFileName = "dump.txt";
+
         /// <summary>
         /// The folder that will be logged to.
         /// </summary>
@@ -32,6 +40,79 @@ namespace DebateScheduler
         /// </summary>
         private static string logFileName = "logs";
 
+        
+        /// <summary>
+        /// Gets the connection string that can be used to connection to the user table within the database.
+        /// </summary>
+        /// <returns>Returns the connection string to the user table in the database.</returns>
+        private static string GetConnectionStringUsersTable()
+        {
+            return ConfigurationManager.ConnectionStrings["UserTable"].ConnectionString;
+        }
+
+        /// <summary>
+        /// Authenticates the username and password combo.
+        /// </summary>
+        /// <param name="username">The username, case sensitivity does not matter.</param>
+        /// <param name="password">The password, case sensitive.</param>
+        public static void AuthenticateUsernamePassword(string username, string password) //TODO: Send a session or something back.. I don't know, send data back adequete enough to do login.
+        {
+            username = username.ToUpperInvariant(); //The username is converted to the upper invariant (upper case) to prevent case sensitivity on usernames.
+
+            SqlConnection connection = new SqlConnection(GetConnectionStringUsersTable());
+
+            try
+            {
+                using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Users WHERE Name LIKE '%" + username + "%'", connection))
+                {
+                    DataTable resultingTable = new DataTable();
+                    adapter.Fill(resultingTable);
+                    //if (resultingTable.)
+                    if (resultingTable.Rows.Count > 0)
+                    {
+                        DataRow userRow = resultingTable.Rows[0]; //If there is more than 1 row, then we've got a problem because there's more than 1 matching username.
+                        string matchedName = userRow["Name"] as string;
+                        string matchedPassword = userRow["Password"] as string;
+                        int matchedPermissions = (int)userRow["Permissions"];
+
+                        if (matchedName == username && password == matchedPassword) //Compares the username and password to the username and password found in the database.
+                        {
+                            //Log the user in, as the username and password match.
+                        }
+                        else
+                        {
+                            //Username/Password did not match send message.
+                        }
+
+                    }
+                    else
+                    {
+                        //The username does not exists.
+                        //TODO: Send a prompt or message to the username letting them know either the username/password did not work.
+                    }
+                    
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                LogException(e, "exception occured while authenticating username/password.");
+            }
+            finally
+            {
+                connection.Close(); //The connection is guranteed a close within the finally code block.
+            }
+        }
+
+        /// <summary>
+        /// Gets the path to the App_Data folder which contains data for the application.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetAppDataPath()
+        {
+            return HttpContext.Current.Server.MapPath("~/App_Data/");
+        }
 
 
         /// <summary>
@@ -39,8 +120,9 @@ namespace DebateScheduler
         /// </summary>
         private static void CreateLogPath()
         {
-            string assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            //string assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             //string assemblyPath = Environment.CurrentDirectory;
+            string assemblyPath = GetAppDataPath();
             fullLogPath = assemblyPath += "\\" + logFolderPath + "\\";
         }
 
@@ -49,7 +131,7 @@ namespace DebateScheduler
         /// </summary>
         /// <param name="sessionID">The session ID of the user who is editing the database.</param>
         /// <param name="message">The message to log.</param>
-        public static void Log(string sessionID, string message)
+        private static void Log(string sessionID, string message)
         {
             if (fullLogPath == string.Empty) //there is no full path, so one must be constructed.
             {
@@ -69,11 +151,33 @@ namespace DebateScheduler
             }
             catch (Exception e)
             {
-                //An error has occured and should be logged.. except this is the log method!
+                LogException(e, "exception occured while logging."); //An error has occured and should be logged.
             }
 
         }
 
+        /// <summary>
+        /// Logs exceptions to the log folder.
+        /// </summary>
+        /// <param name="e">The exception whose message will be logged.</param>
+        /// <param name="info">Additional information to log along with the exception, it is generally a good idea to include where the exception occured. IE: logging in, editing data, etc...</param>
+        private static void LogException(Exception e, string info)
+        {
+            try
+            {
+                Directory.CreateDirectory(fullLogPath);
+
+                using (StreamWriter writer = new StreamWriter(exceptionFileName + exceptionFileName, true))
+                {
+                    writer.WriteLine("Exception with message  \"" + e.Message + "\" was found. Additional Info: \"" + info + "\" logged at " + DateTime.Now); //Logs the message to the exception file.
+                }
+            }
+            catch (Exception exception)
+            {
+                //Well an exception occured, but it cannot be logged since this is the method for logging it. In this case we do nothing really.
+            }
+
+        }
         
 
     }
--- DebateScheduler/MasterPage.Master.cs
+++ DebateScheduler/MasterPage.Master.cs
@@ -11,12 +11,14 @@ namespace DebateScheduler
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //Setting up the server side backend for database related stuffs
+            AppDomain.CurrentDomain.SetData("DataDirectory", DatabaseHandler.GetAppDataPath());
 
         }
 
         protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
         {
-
+            DatabaseHandler.AuthenticateUsernamePassword(Login1.UserName, Login1.Password);
         }
     }
 }
